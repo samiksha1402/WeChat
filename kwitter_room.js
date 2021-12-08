4d86e1f7cc7d688f08688a8e425db14c3cba890e
samiksha1402 <80887833+samiksha1402@users.noreply.github.com>
2021-12-08 12:27:02 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -28,9 +28,9 @@ var firebaseConfig = {
 
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
-      //Start code
-
-      //End code
+       console.log("room_names - " + room_names );
+       row = "<div class='room_name' id="+room_names+" onclick='redirectTo_roomname(this.id)'> #" + room_names + "</div> <hr>";
+       document.getElementById("output").innerHTML += row;
       });});}
 getData();
 
@@ -39,3 +39,9 @@ function logout(){
       localStorage.removeItem("room_name");
       window.location = "index.html";
 }
+
+function redirectTo_roomname(name){
+      console.log(name);
+      localStorage.setItem("room_name", name);
+      window.location = "kwitter_page.html";
+      }
